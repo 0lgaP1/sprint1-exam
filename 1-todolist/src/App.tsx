284548c06f5b6ddd252f70ffcb02bb5339c6d1a9
0lgaP1 <volhapatapchuk@gmail.com>
2024-09-23 13:46:12 +0200

    LocalStorage setItem, getItem, clear(), remove()

--- 1-todolist/src/App.tsx
+++ 1-todolist/src/App.tsx
@@ -5,11 +5,34 @@ import {Button} from "./Btn";
 function App() {
 
     const [counter, setCounter] = useState<number>(0);
+
+    const setLocalStorage = () => {
+        localStorage.setItem("counterValue", JSON.stringify(counter))
+    }
+
+    const getLocalStorage = () => {
+        let valueAsString = localStorage.getItem("counterValue")
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setCounter(newValue)
+        }
+    }
+
+    const clearLocalStorage = () => {
+        localStorage.clear();
+        setCounter(0);
+    }
+
+    const removeItemFromLocStorHandler = () => {
+        localStorage.removeItem("counterValue + 1")
+    }
+
     const incrementCounter = () => {
         if (counter < 5) {
             setCounter(counter + 1);
         }
     };
+
     const resetCounter = () => {
         setCounter(0);
     };
@@ -31,6 +54,24 @@ function App() {
                     className={counter === 0 ? 'disabled' : ''}
                     onClick={resetCounter}
                     disabled={counter === 0}/>
+            </div>
+            <div className="btn-local-storage-div">
+                <Button
+                    name="setLocalStorage"
+                    onClick={setLocalStorage}
+                    className={"setLocalStorage"}/>
+                <Button
+                    name="getLocalStorage"
+                    onClick={getLocalStorage}
+                    className={"getLocalStorage"}/>
+                <Button
+                    name="clearLocalStorage"
+                    onClick={clearLocalStorage}
+                    className={"clearLocalStorage"}/>
+                <Button
+                    name="removeItemFromLocStor"
+                    onClick={removeItemFromLocStorHandler}
+                    className={"removeItemFromLocStor"}/>
             </div>
         </div>
     );
